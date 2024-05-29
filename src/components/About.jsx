f191fe57315c028f6ba514f43b2af6073d09f478
mhmtkarakas <m.karakas32@gmail.com>
2024-05-29 09:59:44 +0300

    about && contact component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import pictureAbout from "../assets/aboutpicture.jpg";
+import "../styles/About.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${pictureAbout})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1>Hakkimizda</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis maiores
+          molestias molestiae quis deserunt ut autem nam explicabo assumenda,
+          odio temporibus vero aperiam doloribus excepturi aliquam rem corrupti
+          ipsum nostrum soluta nemo quibusdam cum praesentium harum hic.
+          Accusantium qui veniam necessitatibus laborum similique. Quam itaque
+          cumque labore, dignissimos, officiis dicta qui, aliquid excepturi
+          perspiciatis modi error nisi delectus ab magnam odio dolorum aut. Est
+          corporis, fuga nihil facere accusamus pariatur totam ab. Fugiat natus
+          eum dolorum corrupti nemo modi dicta. Animi cumque et ut consequuntur
+          officiis omnis, rem ducimus illum nihil velit, aliquid debitis
+          voluptatibus rerum ullam pariatur, accusantium alias! Lorem, ipsum
+          dolor sit amet consectetur adipisicing elit. Ipsam debitis sit id
+          earum eius ipsa consequatur quibusdam vero fuga impedit? Hic qui eius
+          officiis dolorem ipsa accusamus ipsam odio vel quam consequatur
+          tempora, sint quos voluptatum alias natus sit aspernatur ratione
+          officia. Dicta delectus, magni velit facilis error quam a?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laborum
+          magni neque doloremque voluptatibus voluptate. Dignissimos, omnis!
+          Exercitationem corrupti, aspernatur possimus sit enim, rem veritatis
+          voluptate magni alias error eius accusamus blanditiis vitae inventore
+          fugiat explicabo totam tempore sapiente tempora, non dolorem esse
+          quasi! Placeat accusamus, magni, totam architecto repudiandae
+          delectus, veritatis corporis recusandae reprehenderit at rerum maiores
+          voluptatum quibusdam. Veritatis ea cupiditate quam? Similique,
+          reiciendis porro magni soluta deserunt quod excepturi non fuga
+          consequuntur saepe harum ea alias, asperiores laboriosam esse. Natus
+          debitis saepe provident inventore vel distinctio unde obcaecati labore
+          accusantium, a dignissimos quidem nam, repudiandae expedita officia
+          odit nulla possimus sit illo sapiente pariatur vero. Praesentium
+          voluptatum doloribus aspernatur! Sequi fugiat facere repellendus
+          architecto consequatur nemo voluptatibus quidem neque nobis? Natus
+          tempora doloremque at sint recusandae consequatur sit numquam, maiores
+          nihil ipsa provident voluptate saepe harum iusto suscipit a quidem
+          odio tempore dolores? Ipsam ipsum suscipit commodi vero?
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import "../styles/Contact.css";
+import nikeshop from "../assets/nikeshop.jpg";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="contact">
+      <div
+        className="leftSide"
+        style={{ backgroundImage: `url(${nikeshop})` }}
+      ></div>
+      <div className="rightSide">
+        <h1>Bizimle Iletisime Gecin</h1>
+        <form>
+          <label>Ad Soyad</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Lutfen adinizi giriniz..."
+          />
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="Lutfen emailinizi giriniz..."
+          />
+           <label>Mesajiniz</label>
+          <textarea
+            row="6"
+            name="message"
+            placeholder="Lutfen mesajinizi giriniz..."
+          ></textarea>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
